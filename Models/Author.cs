87995afc32de03b96a532361a662c43d6b881757
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 14:38:21 -0600

    🏷️ Crear modelos de datos

---
+++ Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace GraphQL_Learning.Models
+{
+    [Index(nameof(Name), IsUnique = true]
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Name es requerido")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "El campo Name debe tener entre 5 y 100 caracteres.")]
+        public string Name { get; set; } = string.Empty;
+        public List<Book> Books { get; set; } = new List<Book>();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+        public Boolean IsActive { get; set; } = true;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraphQL_Learning.Models
+{
+    [Index(nameof(Title), IsUnique = true)]
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo Title es requerido")]
+        [StringLength(250, MinimumLength = 5, ErrorMessage = "El campo TItle no debe tener entre 5 y 250 caracteres.")]
+        public string Title { get; set; } = string.Empty;
+        public DateOnly PublishedOn { get; set; }
+        public int AuthorId { get; set; }
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+        public Boolean IsActive { get; set; } = true;
+    }
+}
