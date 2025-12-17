9f75f7a59e81f256b87c4a879edd5b91bb64de8a
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 15:10:51 -0600

    👔 Crear servicio para listar los servicios en la carpeta Service

---
+++ Service/ServiceMapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace GraphQL_Learning.Service
+{
+    public class ServiceMapper
+    {
+        public static Type[] GetServiceTypes()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var types = assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsInterface && !t.IsAbstract && !t.IsSealed && t.Name.EndsWith("Service"))
+                .ToArray();
+        }
+    }
+}
