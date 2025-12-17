4cdb35d968b3f6f28ae2d6f499782ad3886e04be
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 15:04:33 -0600

    👔 Crear servicio de autores

---
+++ Service/AuthorService.cs
@@ -0,0 +1,66 @@
+﻿using GraphQL_Learning.Models;
+using GraphQL_Learning.Models.Input;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_Learning.Service
+{
+    public class AuthorService
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorService(AppDbContext context)
+        {
+            _context = context; 
+        }
+
+        public async Task<Author?> GetAuthorAsync(int id)
+        {
+            return await _context.Authors.Include(a => a.Books)
+                .Where(a => a.Id == id).FirstOrDefaultAsync();
+        }
+
+        public IQueryable<Author> GetAuthors()
+        {
+            return _context.Authors.Include(a => a.Books);
+        }
+
+        public async Task<Author> AddAuthorAsync(AddAuthorInput input)
+        {
+            Author author = new()
+            {
+                Name = input.Name
+            };
+            await _context.Authors.AddAsync(author);
+            await _context.SaveChangesAsync();
+            return author;
+        }
+
+        public async Task<Author?> UpdateAuthorAsync(UpdateAuthorInput input)
+        {
+            Author? author = _context.Authors.FindAsync(input.id);
+            if (author == null) { 
+                return null
+            }
+
+            if (!string.IsNullOrEmpty(author.Name))
+            {
+                author.Name = input.Name;
+                author.UpdatedAt = DateTime.Now;
+            }
+            await _context.SaveChangesAsync();
+            return author;
+        }
+
+        public Task<bool> DeleteAuthorAsync(int id)
+        {
+            Author? author = await _context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return false;
+            }
+            author.IsActive = false;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
