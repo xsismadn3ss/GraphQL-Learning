5842cd8d3ce71e95c511e89fe8ea070f0aa7f61b
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 15:14:46 -0600

    ♻️ Actualizar importaciones

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using GraphQL_Learning.Models;
+using GraphQL_Learning.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,13 @@ builder.Configuration.AddUserSecrets<Program>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Agregar servicios al contenedor.
+
+// registrar servicios dinamicamente
+foreach (var type in TypesMapper.GetServiceTypes())
+{
+    builder.Services.AddScoped(type);
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString),
     ServiceLifetime.Scoped);
