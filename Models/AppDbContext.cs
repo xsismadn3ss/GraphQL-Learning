f68ece1e92fc56a5cfafd691265bffebde0626b3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 14:40:21 -0600

    🗃️ Agregar modelos al Db Context

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -7,5 +7,7 @@ namespace GraphQL_Learning.Models
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         // agregar DbSet
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
