f41d180fb273fe59c9651cab83ef80d6edc47a7e
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 14:31:02 -0600

    🔧 Configurar db context en la aplicación

--- Program.cs
+++ Program.cs
@@ -1,34 +1,20 @@
+using GraphQL_Learning.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Agregar secretos de usuario (dotnet user-secrets)
+builder.Configuration.AddUserSecrets<Program>();
+
+// Obtener cadena de conexion
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Agregar servicios al contenedor.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString),
+    ServiceLifetime.Scoped);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
