a3ecf290a1f0416477e2b9431cc15d6461e22510
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-17 14:30:23 -0600

    🗃️ Configurar Db Context

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_Learning.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // agregar DbSet
+    }
+}
