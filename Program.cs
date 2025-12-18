4cdcdcd7a7131581ca144ad1da006e488cf1af17
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-18 10:12:19 -0600

    ✨ Configurar graphql y agregar tipos al esquema

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Agregar secretos de usuario (dotnet user-secrets)
 builder.Configuration.AddUserSecrets<Program>();
 
+var isDevelopmnet = builder.Environment.IsDevelopment();
+
 // Obtener cadena de conexion
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -18,11 +20,34 @@ foreach (var type in TypesMapper.GetServiceTypes())
     builder.Services.AddScoped(type);
 }
 
+// Configurar Db Context
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString),
     ServiceLifetime.Scoped);
 
+// Obtener tipos GraphQL
+Type[] types = [..TypesMapper.GetQueriesTypes()
+    .Union(TypesMapper.GetMutationsTypes()
+    .Union(TypesMapper.GetSubscriptionsTypes()))];
+
+// Configurar GraphQL
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType(q => q.Name("query"))
+    .AddMutationType(m => m.Name("query"))
+    .AddTypes(types)
+    .AddFiltering()
+    .AddSorting()
+    .AddProjections()
+    .ModifyRequestOptions(options => options.IncludeExceptionDetails = isDevelopmnet);
 
 var app = builder.Build();
+
+app.MapGraphQL().WithOptions(new()
+{
+    Tool = { Enable = isDevelopmnet },
+    EnableGetRequests = true,
+    EnableSchemaRequests = true
+});
 
 app.Run();
