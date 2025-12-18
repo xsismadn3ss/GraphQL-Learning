b6df74d1edefc313a10c11f528f8f318f7008fe2
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-18 10:11:36 -0600

    ♻️ Actualizar TypesMapper para obtener mutations, queries y subscriptions

--- Service/TypesMapper.cs
+++ Service/TypesMapper.cs
@@ -6,17 +6,24 @@ namespace GraphQL_Learning.Service
     /// Mapper de tipos, esta clase permite obtener un listado de tipos específico
     /// usando reflexión.
     /// 
-    /// Esto es útil para evitar registrar servicios manualmente
+    /// Esto es útil para evitar registrar servicios o tipos manualmente
     /// </summary>
     public class TypesMapper
     {
         public static Type[] GetServiceTypes()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
+            => [.. typeof(Program).Assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsInterface && !t.IsAbstract && t.Name.EndsWith("Service"))];
 
-            var types = assembly.GetTypes()
-                .Where(t => t.IsClass && !t.IsInterface && !t.IsAbstract && !t.IsSealed && t.Name.EndsWith("Service"))
-                .ToArray();
-        }
+        public static Type[] GetMutationsTypes()
+            => [.. typeof(Program).Assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsInterface && !t.IsAbstract && t.Name.EndsWith("Mutation"))];
+
+        public static Type[] GetQueriesTypes()
+            => [.. typeof(Program).Assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsInterface && !t.IsAbstract && t.Name.EndsWith("Query"))];
+
+        public static Type[] GetSubscriptionsTypes()
+            => [.. typeof(Program).Assembly.GetTypes()
+                .Where(t => t.IsClass && !t.IsInterface && !t.IsAbstract && t.Name.EndsWith("Subscription"))];
     }
 }
