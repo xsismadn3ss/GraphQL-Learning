c17bcb5872053d5a0eace6a88bb406cdf25164ff
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-19 10:00:11 -0600

    📝 Crear Excepciones personalizadas

---
+++ Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQL_Learning.Exceptions
+{
+    public class DuplicateEntityException:Exception
+    {
+        public DuplicateEntityException(string message) : base(message) { }
+
+        public DuplicateEntityException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
