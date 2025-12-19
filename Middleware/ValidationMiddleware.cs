f0240c18000e3ca555cd032235a3f3fa0347533d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-19 08:40:10 -0600

    🦺 Crear middleware para validar inputs

---
+++ Middleware/ValidationMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using HotChocolate.Resolvers;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraphQL_Learning.Middleware
+{
+    public class ValidationMiddleware
+    {
+        private readonly FieldDelegate _next;
+
+        public ValidationMiddleware(FieldDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(IMiddlewareContext context)
+        {
+            foreach (var arg in context.Selection.Field.Arguments)
+            {
+                var value = context.ArgumentValue<object>(arg.Name);
+                if (value != null)
+                {
+                    var ctx = new ValidationContext(value);
+                    var results = new List<ValidationResult>();
+
+                    if (!Validator.TryValidateObject(value, ctx, results, true))
+                    {
+                        var errorBuilder = ErrorBuilder.New().
+                            SetMessage("Validation failed")
+                            .SetExtension("code", "VALIDATION_ERROR");
+                        
+                        foreach (var r in results)
+                        {
+                            var field = string.Join(",", r.MemberNames);
+                            var message = r.ErrorMessage ?? "Unkown validation error";
+
+                            errorBuilder.SetExtension(field, message);
+                        }
+                        throw new GraphQLException(errorBuilder.Build());
+                    }
+                }
+            }
+            await _next(context);
+        }
+    }
+}
