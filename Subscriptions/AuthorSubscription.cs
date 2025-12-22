e3fb2da73ebe5b4c349955a6dd93bedb124a3b4f
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-22 14:37:15 -0600

    ✨ Crear subcripciones para libros y autores

---
+++ Subscriptions/AuthorSubscription.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL_Learning.Models;
+
+namespace GraphQL_Learning.Subscriptions
+{
+    [ExtendObjectType("Subscription")]
+    public class AuthorSubscription
+    {
+        [Subscribe]
+        [Topic("onAuthorAdded")]
+        public Author OnAuthorAdded([EventMessage] Author author)
+            => author;
+
+        [Subscribe]
+        [Topic("onAuthorUpdated")]
+        public Author OnAuthorUpdated([EventMessage] Author author)
+            => author;
+    }
+}
---
+++ Subscriptions/BookSubscription.cs
@@ -0,0 +1,28 @@
+﻿using GraphQL_Learning.Models;
+
+namespace GraphQL_Learning.Subscriptions
+{
+    [ExtendObjectType("Subscription")]
+    public class BookSubscription
+    {
+        [Subscribe]
+        [Topic("onBookAdded")]
+        public Book OnBookAdded([EventMessage] Book book)
+            => book;
+
+        [Subscribe]
+        [Topic("onBookUpdated")]
+        public Book OnBookUpdated([EventMessage] Book book)
+            => book;
+
+        [Subscribe]
+        [Topic("onBookAdded_{authorId}")]
+        public Book OnBookCreatedByAUthor(int authorId, [EventMessage] Book book)
+            => book;
+
+        [Subscribe]
+        [Topic("onBookUpdated_{authorId}")]
+        public Book OnBookUpdatedByAUthor(int authorId, [EventMessage] Book book)
+            => book;
+    }
+}
