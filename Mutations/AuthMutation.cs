32980096bd79bd53ee0c24f5ba147407fa0c0d53
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-22 16:53:27 -0600

    ✨ Crear mutacion para autenticacion

---
+++ Mutations/AuthMutation.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL_Learning.Models.Input;
+using GraphQL_Learning.Models.Output;
+using GraphQL_Learning.Services;
+
+namespace GraphQL_Learning.Mutations
+{
+    [ExtendObjectType("Mutation")]
+    public class AuthMutation
+    {
+        public async Task<CredentialsAuthOutput> Login(
+            LoginAuthInput input,
+            [Service] AuthService authService)
+               => await authService.LoginAsync(input);
+    }
+}
