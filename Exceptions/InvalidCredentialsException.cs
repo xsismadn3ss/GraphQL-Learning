5698fe46d99eebedc964e9acf4d215ddde32e2d9
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-22 16:51:28 -0600

    🏷️ Crear exepcion personalizada para credenciales incorrectas

---
+++ Exceptions/InvalidCredentialsException.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQL_Learning.Exceptions
+{
+    public class InvalidCredentialsException : Exception
+    {
+        public InvalidCredentialsException(string message) : base(message) { }
+
+        public InvalidCredentialsException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
