cba32c22289562747a62d8a0b432a3277bbe1789
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-22 16:27:01 -0600

    🏷️ Crear tipos para autenticacion

---
+++ Models/Input/AuthInputs.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraphQL_Learning.Models.Input
+{
+    public record class LoginAuthInput
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public record class RegisterAuthInput
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(255, MinimumLength = 8)]
+        [PasswordPropertyText]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public record class UpdateAuthInput
+    {
+        [StringLength(100, MinimumLength = 3)]
+        public string? Name { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string? Username { get; set; }
+        [StringLength(100)]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [StringLength(255, MinimumLength = 8)]
+        [PasswordPropertyText]
+        public string? Password { get; set; }
+    }
+}
---
+++ Models/Output/AuthOutputs.cs
@@ -0,0 +1,20 @@
+﻿namespace GraphQL_Learning.Models.Output
+{
+    public record class UserAuthOutput
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    public record class CredentialsAuthOutput
+    {
+        public string Username { get; set; } = string.Empty;
+        public string AuthToken { get; set; } = string.Empty;
+    }
+}
