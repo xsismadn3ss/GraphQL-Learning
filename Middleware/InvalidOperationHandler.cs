594f41a6d4caf5a85bf09c728fa3a43bcfc25264
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-23 09:03:47 -0600

    🌱 Implementar middleware para manejar excepciones de operacion invalida

---
+++ Middleware/InvalidOperationHandler.cs
@@ -0,0 +1,30 @@
+﻿using HotChocolate.Resolvers;
+
+namespace GraphQL_Learning.Middleware
+{
+    public class InvalidOperationHandler: IGraphMiddleware
+    {
+        private readonly FieldDelegate _next;
+
+        public InvalidOperationHandler(FieldDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(IMiddlewareContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+             catch (InvalidOperationException ex) {
+                context.ReportError(
+                    ErrorBuilder.New()
+                    .SetMessage(ex.Message)
+                    .SetCode("INVALID OPERATION")
+                    .SetExtension("timestamp", DateTime.Now)
+                    .Build());
+            }
+        }
+    }
+}
