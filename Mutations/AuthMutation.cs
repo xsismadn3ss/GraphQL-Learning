08784eb4f8ff6c8f2e6b775b722820238d8ae8fb
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-12-23 10:44:02 -0600

    ✨ Agregar asinación de cookies al iniciar o registrarse

--- Mutations/AuthMutation.cs
+++ Mutations/AuthMutation.cs
@@ -7,14 +7,37 @@ namespace GraphQL_Learning.Mutations
     [ExtendObjectType("Mutation")]
     public class AuthMutation
     {
+        private static void SetTokenCookie(IHttpContextAccessor httpContext, string token)
+        {
+            var cookieOptions = new CookieOptions()
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTime.UtcNow.AddHours(2)
+            };
+            httpContext.HttpContext?.Response.Cookies.Append("auth-token", token, cookieOptions);
+        }
         public async Task<CredentialsAuthOutput> Login(
             LoginAuthInput input,
-            [Service] AuthService authService)
-               => await authService.LoginAsync(input);
+            [Service] AuthService authService,
+            [Service] IHttpContextAccessor httpContext)
+        {
+
+            var credentials = await authService.LoginAsync(input);
+            SetTokenCookie(httpContext, credentials.AuthToken);
+            return credentials;
+        }
 
         public async Task<CredentialsAuthOutput> Register(
             RegisterAuthInput input,
-            [Service] AuthService authService)
-            => await authService.RegisterAsync(input);
+            [Service] AuthService authService,
+            [Service] IHttpContextAccessor httpContext)
+        {
+
+            var credentials = await authService.RegisterAsync(input);
+            SetTokenCookie(httpContext, credentials.AuthToken);
+            return credentials;
+        }
     }
 }
